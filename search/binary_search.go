978fa0e9f70bec2662d504c7501ddb1f2a50d7f3
Damir Kadyrov <dizzarg@mail.ru>
2019-03-31 22:36:42 +0300

    fix problem when element not found in array

--- search/binary_search.go
+++ search/binary_search.go
@@ -10,16 +10,16 @@ import (
 // returns position of found element
 // otherwise -1
 func Search(arr []int, el int) int {
-	hi := len(arr)
+	hi := len(arr) - 1
 	lo := 0
-	for lo < hi {
+	for lo <= hi {
 		mid := (hi + lo) / 2
 		if arr[mid] == el {
 			return mid
 		} else if arr[mid] < el {
-			lo = mid
+			lo = mid + 1
 		} else {
-			hi = mid
+			hi = mid - 1
 		}
 	}
 	return -1
