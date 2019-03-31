e51095710e68d5806c31ece4958219529551a444
Damir Kadyrov <dizzarg@mail.ru>
2019-03-31 22:38:20 +0300

    remove main method

--- search/binary.go
+++ search/binary.go
@@ -1,11 +1,5 @@
 package main
 
-import (
-	"log"
-	"math/rand"
-	"time"
-)
-
 // Iterative binary search implementation
 // returns position of found element
 // otherwise -1
@@ -23,23 +17,4 @@ func Search(arr []int, el int) int {
 		}
 	}
 	return -1
-}
-
-func main() {
-	log.Println("Binary search algorithm demo")
-	size := 1000
-	arr := make([]int, size)
-	for i := 0; i < size; i++ {
-		arr[i] = i
-	}
-	rand.Seed(time.Now().Unix())
-	element := rand.Intn(size)
-	log.Printf("Try find element %d in range %d to %d", element, arr[0], arr[len(arr)-1])
-	pos := Search(arr, element)
-	if pos != -1 {
-		log.Printf("Found element %d by index %d", element, pos)
-	} else {
-		log.Printf("Element %d not found in array", element)
-	}
-
 }
