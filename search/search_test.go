c82f1a773a1117129c70bfd36468be2e7ed8e4c9
Damir Kadyrov <dizzarg@mail.ru>
2019-03-31 23:01:26 +0300

    add benchmarks

--- search/search_test.go
+++ search/search_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"math/rand"
+	"testing"
+)
 
 var testArray = [9]int{10, 20, 30, 40, 50, 60, 70, 80, 90}
 
@@ -51,5 +54,33 @@ func TestLinearSearch_nothingFoundInEmptyArray(t *testing.T) {
 	actualIndex := LinearSearch(testArray[:], 0)
 	if actualIndex != expectedIndex {
 		t.Errorf("For %v expected %d, but got %d", testArray, expectedIndex, actualIndex)
+	}
+}
+
+func BenchmarkBinarySearch1KFindRandom(b *testing.B) {
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		data := make([]int, 1<<10)
+		for i := 0; i < len(data); i++ {
+			data[i] = i
+		}
+		el := data[rand.Int31n(1<<10)]
+		b.StartTimer()
+		BinarySearch(data, el)
+		b.StopTimer()
+	}
+}
+
+func BenchmarkLinearSearch1KFindRandom(b *testing.B) {
+	b.StopTimer()
+	for i := 0; i < b.N; i++ {
+		data := make([]int, 1<<10)
+		for i := 0; i < len(data); i++ {
+			data[i] = i
+		}
+		el := data[rand.Int31n(1<<10)]
+		b.StartTimer()
+		LinearSearch(data, el)
+		b.StopTimer()
 	}
 }
