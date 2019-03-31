4e74e72c57e605d05b600bd43f326371f65daa7b
Damir Kadyrov <dizzarg@mail.ru>
2019-03-31 22:35:29 +0300

    coverage with tests binary search algorithm

---
+++ search/binary_search_test.go
@@ -0,0 +1,30 @@
+package main
+
+import "testing"
+
+var testArray = [9]int{10, 20, 30, 40, 50, 60, 70, 80, 90}
+
+func TestSearch_successFound(t *testing.T) {
+	expectedIndex := 3
+	actualIndex := Search(testArray[:], 40)
+	if actualIndex != expectedIndex {
+		t.Errorf("For %v expected %d, but got %d", testArray, expectedIndex, actualIndex)
+	}
+}
+
+func TestSearch_successNotFound(t *testing.T) {
+	expectedIndex := -1
+	actualIndex := Search(testArray[:], 200)
+	if actualIndex != expectedIndex {
+		t.Errorf("For %v expected %d, but got %d", testArray, expectedIndex, actualIndex)
+	}
+}
+
+func TestSearch_nothingFoundInEmptyArray(t *testing.T) {
+	testArray := [0]int{}
+	expectedIndex := -1
+	actualIndex := Search(testArray[:], 0)
+	if actualIndex != expectedIndex {
+		t.Errorf("For %v expected %d, but got %d", testArray, expectedIndex, actualIndex)
+	}
+}
